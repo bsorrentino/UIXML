0bdef2e9d2430916e65a240cf02d2754c29974ed
bsorrentino <bartolomeo.sorrentino@gmail.com>
2012-08-16 15:13:53 +0200

    add sample project

---
+++ Samples/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewDataEntry
+//
+//  Created by Fabrizio Guglielmino on 31/08/10.
+//  Copyright Infit S.r.l 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, nil);
+        return retVal;
+    }
+}
